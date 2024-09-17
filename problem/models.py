ac92550e312b69faec5902e2d205b75311533d24
parkeunjin020404 <eunjin020404@naver.com>
2024-09-17 21:46:11 +0900

    모델 변경, 뷰 변경

--- problem/models.py
+++ problem/models.py
@@ -1,7 +1,12 @@
 from django.db import models
-
+from users.models import CustomUser
 # Create your models here.
 class Problem(models.Model):
+    STATUS_CHOICES = [
+        ('RIGHT', '맞은 문제'),
+        ('WRONG', '틀린 문제'),
+        ('YET', '아직 안 푼 문제'),
+    ]
     question = models.CharField(max_length=300)
     answer = models.CharField(max_length=2)
     solution = models.CharField(max_length=400)
@@ -13,3 +18,10 @@ class Problem(models.Model):
     level = models.CharField(max_length=20)
     chapter = models.CharField(max_length=20)
     point = models.IntegerField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='YET') 
+
+    # problem/models.py
+class UserProblem(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=Problem.STATUS_CHOICES)
